f8e2dfc91b44844d577a5a4be4a9f00586926f05
SobusaDriver <kenlallemand@gmail.com>
2023-05-29 14:47:39 -0500

    updated mock data format

--- app/Screens/ScreenHome.tsx
+++ app/Screens/ScreenHome.tsx
@@ -1,11 +1,6 @@
-import {StyleSheet, Text, View} from 'react-native';
+import { View} from 'react-native';
 import React from 'react';
-import Antwerp from '../../meals_mock_api/antwerp.json';
-import Chicago from '../../meals_mock_api/chicago.json';
-import SanFrancisco from '../../meals_mock_api/san_francisco.json';
-import Toronto from '../../meals_mock_api/toronto.json';
 import BusinessType from '../Types/BusinessType';
-import MaterialCard from '../Components/MaterialCard';
 import ListOfCards from '../Components/ListOfCards';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import { listOfBusiness } from '../Utils/BusinessUtils';
