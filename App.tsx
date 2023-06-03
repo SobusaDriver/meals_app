5d92d34d63c4925c19c095f99d1d6c7f77a274df
SobusaDriver <kenlallemand@gmail.com>
2023-06-03 16:59:41 -0500

    cleanup of code

--- App.tsx
+++ App.tsx
@@ -8,13 +8,8 @@ import {enableLatestRenderer} from 'react-native-maps';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import {useColorScheme} from 'react-native';
-import ScreenRestaurants from './app/Screens/ScreenRestautants';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import ScreenHotels from './app/Screens/ScreenHotels';
 import Nestedhome from './app/ScreenNavigators/NestedHome';
-import NestedNavigatorBeta from './app/ScreenNavigators/NestedNavigatorBeta';
-import {listOfNestedPages} from './app/NavigatorsParamsTypes/NestedPagesProps';
 import NestedRestaurant from './app/ScreenNavigators/NestedRestaurant';
 import NestedHotel from './app/ScreenNavigators/NestedHotel';
 
@@ -55,39 +50,6 @@ function App(): JSX.Element {
             ),
           }}
         />
-        {/* <Tab.Screen
-          name="Restaurants"
-          component={
-            <NestedNavigatorU
-              headerText="Restaurant"
-              title={listOfNestedPages.RestaurantNested}
-              page={ScreenRestaurants}
-            />
-            // NestedNavigatorU({
-            //   headerText: 'Restaurant',
-            //   title: listOfNestedPages.RestaurantNested,
-            //   page: ScreenRestaurants,
-            // })
-          }
-          options={{
-            title: 'Restaurants',
-            headerShown: false,
-            tabBarIcon: ({color, size}) => (
-              <Icon name="restaurant" color={color} size={size} />
-            ),
-          }}
-          // initialParams={
-          //   headerText: 'Restaurant',
-          //   title: listOfNestedPages.RestaurantNested,
-          //   page: ScreenRestaurants,
-          // }
-        >
-          <NestedNavigator
-            headerText="Restaurant"
-            title={listOfNestedPages.RestaurantNested}
-            page={ScreenRestaurants}
-          />
-        </Tab.Screen> */}
       </Tab.Navigator>
     </NavigationContainer>
   );
